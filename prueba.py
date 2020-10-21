d7caa4fc84ea6190752ec5d09e55358e06b25b13
MarlonJaraZ <marlonja@hotmail.com>
2020-10-21 18:52:47 -0500

    segundo comentario

--- prueba.py
+++ prueba.py
@@ -2,7 +2,12 @@
 # esperemos que todo funciones correctamente
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 x = pd.read_csv("datos/cars.csv")
 
 print(x)
+
+# seleccion
+sns.countplot(x["drives_right"])
