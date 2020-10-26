3c1b253f707c669f746163b82a859b5636304172
MarlonJaraZ <marlonja@hotmail.com>
2020-10-26 15:25:29 -0500

    otro ejemplo

--- list_comprehension.py
+++ list_comprehension.py
@@ -20,3 +20,22 @@ print(list_tercera)
 # son iguales?
 print(tercera_letra == list_tercera)
 
+###### Otro Ejemplo #########
+
+# list comprehension 
+
+pair2 = [(num1, num2) for num1 in range (0,2) for num2 in range(6,8)] 
+print(pair2)
+
+# condicional list comprehension
+# podemos agregar condicionales IF en el "iterable" o en el "output exp"
+
+# iterable
+
+[num_cond ** 2 for num_cond in range(10) if num_cond % 2 == 0]
+# devuelve los cuadrados hasta el numero 10 unicamente de los numeros pares.
+
+# en output exp
+
+[num_cond2 ** 2 if num_cond2 % 2 == 0 else 0 for num_cond2 in range (10)]
+# devuelve los cuadrados hasta el 10, si el resultado es impar, regresa 0
